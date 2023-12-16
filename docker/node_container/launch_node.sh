dd8b8d9d14f537c347f4032a6a3de52936565f36
Mathias Ciliberto <mathias.ciliberto@gmail.com>
2023-12-16 09:54:21 +0000

    Added dev flag to docker node launcher command to avoid hanging situation

--- docker/node_container/launch_node.sh
+++ docker/node_container/launch_node.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/decentralml/node-decentralml --rpc-external --rpc-methods=Unsafe --prometheus-external --rpc-cors all > /dev/null
+/decentralml/node-decentralml --dev --rpc-external --rpc-methods=Unsafe --prometheus-external --rpc-cors all > /dev/null
