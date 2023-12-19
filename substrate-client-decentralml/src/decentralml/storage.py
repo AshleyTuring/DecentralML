a67a99e757cc1b2ea3953aaa92b801f247b93c71
Mathias Ciliberto <mathias.ciliberto@gmail.com>
2023-12-19 18:29:55 +0100

    Fixed creation of folders when uploading

--- substrate-client-decentralml/src/decentralml/storage.py
+++ substrate-client-decentralml/src/decentralml/storage.py
@@ -7,8 +7,8 @@ from os.path import join
 from .settings import ASSETS_FOLDER, REMOTE_FOLDER
 
 def upload_data(role_folder, task_id):
-    remote_folder = join(REMOTE_FOLDER, task_id, role_folder)
-    os.mkdir(remote_folder)
+    remote_folder = join(REMOTE_FOLDER, str(task_id), role_folder)
+    os.makedirs(remote_folder)
     files = [f for f in glob.glob(join(ASSETS_FOLDER, role_folder)+"**")]
     for old_f in files:
         new_f = old_f.replace(ASSETS_FOLDER, f"{REMOTE_FOLDER}/{task_id}")
