671d7f962f762b99a23d9f9e9614106686c43dac
Mathias Ciliberto <mathias.ciliberto@gmail.com>
2023-12-19 18:08:38 +0100

    Added settings for remote folder

--- substrate-client-decentralml/src/decentralml/settings.py
+++ substrate-client-decentralml/src/decentralml/settings.py
@@ -1,11 +1,15 @@
 import os
 
 SOCKET_URL = None
+
 ASSETS_FOLDER = None
+REMOTE_FOLDER = None
+
 IPFS_END_POINT = None
 IPFS_API_KEY = None
 IPFS_API_SECRET = None
 EMULATE_IPFS = None
+DECENTRALML_ROLE = None
 
 try:
     SOCKET_URL = os.environ["SOCKET_URL"]
@@ -16,7 +20,13 @@ try:
     ASSETS_FOLDER = os.environ["ASSETS_FOLDER"]
 except KeyError:
     working_directory = os.path.expanduser("~")
-    ASSETS_FOLDER = os.path.join(working_directory, 'DecentralML', 'assets')
+    ASSETS_FOLDER = os.path.join(working_directory, 'decentralml', 'assets')
+
+try:
+    REMOTE_FOLDER = os.environ["REMOTE_FOLDER"]
+except KeyError:
+    working_directory = os.path.expanduser("~")
+    REMOTE_FOLDER = os.path.join(working_directory, 'decentralml', 'remote')
 
 try:
     IPFS_END_POINT = os.environ["IPFS_END_POINT"]
@@ -37,6 +47,11 @@ try:
     EMULATE_IPFS = bool(os.environ["EMULATE_IPFS"])
 except KeyError:
     EMULATE_IPFS = True
+
+try:
+    DECENTRALML_ROLE = os.environ["DECENTRALML_ROLE"]
+except:
+    DECENTRALML_ROLE = "DECENTRALML_ROLE"
 
 def load_user_settings():
     pass
