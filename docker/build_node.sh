0c51ceafb715a0e0a371675e2dd7547cb65ca43c
Mathias Ciliberto <mathias.ciliberto@gmail.com>
2023-12-29 17:21:23 +0100

    Cleaned up buiding script for node

--- docker/build_node.sh
+++ docker/build_node.sh
@@ -3,7 +3,4 @@ export DOCKER_BUILDKIT=1
 export BUILDKIT_PROGRESS=plain
 export DOCKER_DEFAULT_PLATFORM=$BUILDPLATFORM
 
-# cargo clean --target-dir ../substrate-node-decentralml/
-# cargo build --manifest-path ../substrate-node-decentralml/Cargo.toml --package node-decentralml --release
-
 docker build --no-cache --pull -t decentralml_node:latest ./node_container/
