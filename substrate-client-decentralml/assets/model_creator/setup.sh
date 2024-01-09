e683b8ed3ec199ae5579e5378d8a06777e166585
Mathias Ciliberto <mathias.ciliberto@gmail.com>
2024-01-09 15:19:02 +0100

    Change model creator script to setup

--- substrate-client-decentralml/assets/model_creator/setup.sh
+++ substrate-client-decentralml/assets/model_creator/setup.sh
@@ -4,6 +4,3 @@ echo "Setting up environment for model creator"
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r ./requirements.txt
-
-echo "Executing task script"
-python3 -m model_contributor
